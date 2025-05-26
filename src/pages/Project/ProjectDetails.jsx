8797ee6d694cbd0584838dada99de7e1e1d1a1e3
Dipen <dipendraboyaju222@gmail.com>
2025-05-26 22:27:07 +0545

    ch

--- src/pages/Project/ProjectDetails.jsx
+++ src/pages/Project/ProjectDetails.jsx
@@ -11,11 +11,7 @@ const ProjectDetails = () => {
         <div className="absolute -top-5 right-10 animate-bounce">
           <Link to="/signlanguageplatform" className="bg-orange-500 p-5 px-18 rounded-full font-bold font-quicksand uppercase text-xl tracking-wider text-white shadow-lg flex items-center gap-6">Try Demo <CornerDownRight strokeWidth={2.5} /></Link>
         </div>
-        <div className="border-b pb-8 border-zinc-800/20">
-          <h2 className="text-2xl font-bold font-quicksand">Objective</h2>
-          <p className="font-poppins pt-5 text-zinc-800/90 text-lg font-light">Our Sign Language project bridges the communication gap between hearing and non-hearing communities through AI-powered real-time translation. We have started with Nepali Sign Language gradually aiming to connect with other versions. By converting Nepali Sign Language to speech and vice versa, the platform enables seamless interaction across web, mobile, and wearable devices like smart glasses. Designed in close collaboration with the Deaf community, it combines gesture recognition, speech processing, and emotion-aware avatars to ensure natural, accessible, and inclusive communication for all.
-          </p>
-        </div>
+
         <div className="grid grid-cols-6 mt-10 gap-8">
           <div className="col-span-4 ">
             <div className="">
@@ -29,7 +25,7 @@ const ProjectDetails = () => {
               <div className="my-4 border-t border-b py-3 border-zinc-800/20">
                 <p className="flex items-center gap-5 text-zinc-800/80"><span className="uppercase text-purple-500">Division</span> Center for Human Mobility and Communications</p>
               </div>
-              <div className="grid grid-cols-3 mt-5">
+              <div className="grid grid-cols-3 mt-5 border-b border-zinc-800/20 pb-5">
                 <div className="">
                   <h5 className="uppercase text-purple-500 pb-2">Year</h5>
                   <p>2022</p>
@@ -49,6 +45,12 @@ const ProjectDetails = () => {
                 </div>
               </div>
             </div>
+
+            <div className="border-b pb-8 border-zinc-800/20 mt-5">
+              <h2 className="text-2xl font-bold font-quicksand uppercase">Objective</h2>
+              <p className="font-poppins pt-5 text-zinc-800/90 text-lg font-light">Our Sign Language project bridges the communication gap between hearing and non-hearing communities through AI-powered real-time translation. We have started with Nepali Sign Language gradually aiming to connect with other versions. By converting Nepali Sign Language to speech and vice versa, the platform enables seamless interaction across web, mobile, and wearable devices like smart glasses. Designed in close collaboration with the Deaf community, it combines gesture recognition, speech processing, and emotion-aware avatars to ensure natural, accessible, and inclusive communication for all.
+              </p>
+            </div>
           </div>
           <div className="col-span-2">
             <h3 className="text-xl font-semibold text-purple-500">Word Based Nepali Sign Language Dataset</h3>
@@ -62,6 +64,9 @@ const ProjectDetails = () => {
                 <PartnerOrganizationCard name="Khwopa Engineering College" address="Libali, Bhaktapur - 8, Nepal" image="/images/KhEC_Logo.png" />
                 <PartnerOrganizationCard name="Community Based Rehabilitation Organization, Bhaktapur" address="" image="" />
               </div>
+            </div>
+            <div className=" mt-10">
+              <img src="/images/signlanguageteam.png" alt="" />
             </div>
           </div>
         </div>
