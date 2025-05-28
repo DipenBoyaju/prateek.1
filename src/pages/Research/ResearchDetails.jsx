2c78c390e51fbffb7ce5cdaafbe81e185258780f
Dipen <dipendraboyaju222@gmail.com>
2025-05-28 21:51:06 +0545

    ch

--- src/pages/Research/ResearchDetails.jsx
+++ src/pages/Research/ResearchDetails.jsx
@@ -33,11 +33,27 @@ const ResearchDetails = () => {
                 Projects
               </h4>
             </div>
-            <div className="">
-              <Link to="/projects/signlanguage" className="block border-l-4 border-l-emerald-400 rounded shadow p-3 bg-emerald-500/10 border border-emerald-500/30 hover:scale-101 transition-all duration-300 ease-in-out">
-                <p className="tracking-wider text-lg ">Inclusive Real time Sign Language Translation Platform</p>
-                <p className="text-sm font-light text-zinc-800/80"><span className="uppercase font-medium">Year</span> - 2022</p>
-              </Link>
+
+            <div className="flex flex-col gap-3">
+              {
+                data?.projects?.length > 0 ? (
+                  data.projects.map((project, index) => (
+                    <Link
+                      key={index}
+                      to={project.link}
+                      className="block border-l-4 border-l-emerald-400 rounded shadow p-3 bg-emerald-500/10 border border-emerald-500/30 hover:scale-101 transition-all duration-300 ease-in-out"
+                    >
+                      <p className="tracking-wider text-lg">{project.name}</p>
+                      <p className="text-sm font-light text-zinc-800/80">
+                        <span className="uppercase font-medium">Year</span> - 2022
+                      </p>
+                    </Link>
+                  ))
+                ) : (
+                  <p>No projects available</p>
+                )
+              }
+
             </div>
           </div>
           <div className="">
