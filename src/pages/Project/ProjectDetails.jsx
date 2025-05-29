bb6720e24fe4900ae3d63e9723d4c2dbd66d1fb1
Dipen <dipendraboyaju222@gmail.com>
2025-05-29 21:07:19 +0545

    b

--- src/pages/Project/ProjectDetails.jsx
+++ src/pages/Project/ProjectDetails.jsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom"
-import PartnerOrganizationCard from "../../components/PartnerOrganizationCard"
 import Title from "../../components/Title"
-import { CornerDownRight, CornerRightDown } from 'lucide-react'
 
 const ProjectDetails = () => {
   return (
@@ -10,17 +8,25 @@ const ProjectDetails = () => {
       <div className="container mx-auto px-4 md:px-8 py-10 md:py-20 relative">
 
         <div className="pb-8">
-          <h2 className="text-2xl font-bold font-quicksand uppercase text-purple-500">Objective</h2>
+          <h2 className="text-2xl font-bold font-quicksand uppercase text-cyan-400">Objective</h2>
           <p className="font-poppins pt-5 text-zinc-800/90 text-lg font-light">Our Sign Language project bridges the communication gap between hearing and non-hearing communities through AI-powered real-time translation. We have started with Nepali Sign Language gradually aiming to connect with other versions. By converting Nepali Sign Language to speech and vice versa, the platform enables seamless interaction across web, mobile, and wearable devices like smart glasses. Designed in close collaboration with the Deaf community, it combines gesture recognition, speech processing, and emotion-aware avatars to ensure natural, accessible, and inclusive communication for all.
           </p>
         </div>
 
         <div className="border-t border-b py-3 border-zinc-800/20">
-          <p className="flex items-center gap-5 text-zinc-800/80"><span className="uppercase text-purple-500">Division</span> Center for Human Mobility and Communications</p>
+          <p className="flex items-center gap-5 text-zinc-800/80"><span className="uppercase text-cyan-400">Division</span> Center for Human Mobility and Communications</p>
         </div>
 
-        <div className="">
-          <a href="/projects/signlanguage/wordbase">Word base</a>
+        <div className="pt-10 grid sm:grid-cols-2 gap-5">
+          <Link
+            to='/projects/signlanguage/wordbase'
+            className="block border-l-4 border-l-emerald-400 rounded shadow p-3 bg-emerald-500/10 border border-emerald-500/30 hover:scale-101 transition-all duration-300 ease-in-out"
+          >
+            <p className="tracking-wider text-lg">Word Based Nepali Sign Language</p>
+            <p className="text-sm font-light text-zinc-800/80">
+              <span className="uppercase font-medium">Year</span> - 2022
+            </p>
+          </Link>
         </div>
       </div >
     </div >
