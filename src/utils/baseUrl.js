1630c20d90572d74689eae3a7fd8ae8893353ecc
Dipen <dipendraboyaju222@gmail.com>
2025-05-29 21:17:18 +0545

    vg

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
-export const baseUrl = 'http://localhost:5000'
-// export const baseUrl = 'https://prateek-backend.vercel.app'
+// export const baseUrl = 'http://localhost:5000'
+export const baseUrl = 'https://prateek-backend.vercel.app'
 
 
 
