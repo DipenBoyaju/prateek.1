1f0e4d8b27a7a87afa9d5af45285d17bb8d099d2
Dipen <dipendraboyaju222@gmail.com>
2025-05-30 14:37:27 +0545

    ch

--- src/pages/Project/SingleProject.jsx
+++ src/pages/Project/SingleProject.jsx
@@ -15,15 +15,16 @@ const SingleProject = () => {
         <div className="grid md:grid-cols-6 gap-8">
           <div className="md:col-span-4 ">
             <div className="">
-              <div className="shadow rounded-xl overflow-hidden border border-zinc-800/20">
-                <img src="/images/signlanguage.png" alt="" className="hover:scale-105 transition-all duration-300 ease-linear" />
+              <div className=" mt-5 border-b border-zinc-800/20 pb-5">
+                <h3 className="text-xl font-semibold text-purple-500">Word Based Nepali Sign Language Dataset</h3>
+                <p className="font-light mt-3">We created a first word based Nepali Sign Language Dataset that consisted of 4 words: Namaste (Hello), Dhanyabaad (Thank you), Ghar (House) and Ma (me), each word consisting of 5000 unique signed images. </p>
               </div>
               <div className="flex gap-4 items-center mt-5">
                 <p className="text-xl font-semibold uppercase text-purple-400">Project Details</p>
                 <CornerRightDown className="animate-bounce" />
               </div>
               <div className="my-4 border-t border-b py-3 border-zinc-800/20">
-                <p className="flex items-center gap-5 text-zinc-800/80"><span className="uppercase text-purple-500">Division</span> Center for Human Mobility and Communications</p>
+                <p className="flex items-center gap-5 text-zinc-800/80"><span className="uppercase text-purple-500">Division</span> <a href="/division/center-for-human-mobility-and-communications" className="cursor-pointer hover:underline hover:text-cyan-700" >Center for Human Mobility and Communications</a></p>
               </div>
               <div className="grid grid-cols-3 mt-5 border-b border-zinc-800/20 pb-5">
                 <div className="">
@@ -46,20 +47,16 @@ const SingleProject = () => {
               </div>
             </div>
 
-            <div className=" mt-5 border-b border-zinc-800/20 pb-5">
-              <h3 className="text-xl font-semibold text-purple-500">Word Based Nepali Sign Language Dataset</h3>
-              <p className="font-light mt-3">We created a first word based Nepali Sign Language Dataset that consisted of 4 words: Namaste (Hello), Dhanyabaad (Thank you), Ghar (House) and Ma (me), each word consisting of 5000 unique signed images. </p>
-            </div>
           </div>
           <div className="md:col-span-2">
             <div className=" rounded-xl overflow-hidden">
               <h4 className="bg-purple-500 py-2 px-3 text-lg font-semibold text-white">Partner Organizations</h4>
               <div className="flex flex-col gap-3">
                 <PartnerOrganizationCard name="National Deaf federation of Nepal" address="Ranibari Marg, Kathmandu" image="/images/NDFN-Logo.png" />
-                <PartnerOrganizationCard name="Kavre Deaf School" address="" image="" />
-                <PartnerOrganizationCard name="Vidhya Sagar English Secondary School" address="" image="/images/VidhyaSagar_logo.png" />
-                <PartnerOrganizationCard name="Khwopa Engineering College" address="Libali, Bhaktapur - 8, Nepal" image="/images/KhEC_Logo.png" />
-                <PartnerOrganizationCard name="Community Based Rehabilitation Organization, Bhaktapur" address="" image="" />
+                <PartnerOrganizationCard name="Kavre Deaf School" address="Banepa, kavre " image="" />
+                <PartnerOrganizationCard name="Vidhya Sagar English Secondary School" address=" Banepa , Kavrepalanchok" image="/images/VidhyaSagar_logo.png" />
+                <PartnerOrganizationCard name="Khwopa Engineering College" address="Libali, Bhaktapur - 8" image="/images/KhEC_Logo.png" />
+                <PartnerOrganizationCard name="Community Based Rehabilitation Organization" address="Indrayani, Bhaktapur" image="" />
               </div>
             </div>
             <div className=" mt-10">
